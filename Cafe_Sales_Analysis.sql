6ec7c52da7120366a9ee89a64a96974850be95f5
ankit-1213 <ankit.acharya1213@gmail.com>
2026-05-04 20:49:45 +0545

    Add files via upload

---
+++ Cafe_Sales_Analysis.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sales(
+	Date DATE,
+	Item VARCHAR(25),
+	Category VARCHAR(25),
+	Price NUMERIC(10,2),
+	Quantity INT,
+	payment_Method VARCHAR(25),
+	Sales NUMERIC(15,2)
+);
+ SELECT * FROM sales;
